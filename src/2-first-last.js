3f11c84fc8c60b6c3214bff28b6d0bef243b62a8
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-10-11 23:44:48 +0200

    first try

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,22 @@
 export const firstLast = (items) => {
+
+  // should return the first and last items from an array with two items  // 
+  if (items.lenght < 1) {
   return `First: ${items[0]}, Last: ${items[1]}`
+    }
+
+    // should return the first and last items from an array with many items'
+   if  (items[0]]: items[items.length - 1){
+     return 
+   }
+    
+    // 'Should only return the first item if the array only has one item'
+    if else  (items.lenght === 0) {
+      return `${items[0]}`
+    }
+   
+    // // 'Should return "No items!" if the array is empty'
+    // else (items([]) {
+    //   return `('No items!')`
+    // }
 }
--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,20 @@
 export const findIndex = (array, value) => {
-  return
+
+  // it('should return the correct index of the first item', () => {
+  //   expect(findIndex(items, 'Apple')).toEqual(0)
+  return (items[0])
+
+
+  // it('should return the correct index of the last item', () => {
+  //   expect(findIndex(items, 'Lemon')).toEqual(4)
+  return (items[4])
 }
+
+
+// it('should return the correct index of an item in the middle', () => {
+//   expect(findIndex(items, 'Orange')).toEqual(2)
+return 
+
+
+// it('should return -1 if the item is not in the array', () => {
+//   expect(findIndex(items, 'Pineapple')).toEqual(-1)
